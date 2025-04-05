f332cac6ff4aa01a3a678d16cc13bf1a99531e36
Ajay o s <ajayosakhub@gmail.com>
2025-04-05 17:01:25 +0530

    (●'◡'●)

--- js/script.js
+++ js/script.js
@@ -4,16 +4,8 @@
 })(jQuery);
 
 window.onload = function () {
-  // Play the muted audio immediately on page load
-  var audio = document.getElementById("my_audio");
   const input = document.getElementById("whatsappNumber");
   input.value = "+91";
-
-  // After 2 seconds, unmute the audio
-  setTimeout(function () {
-    audio.play();
-    audio.muted = false; // Unmute the audio
-  }, 2000); // 2000 ms = 2 seconds
 };
 
 window.onblur = function () {
